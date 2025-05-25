d2897bbdae9e03b45160bc7cb8e8d8cc7f51cf05
afanasyevanar <afanasyevanar@gmail.com>
2025-05-25 23:43:05 +0700

    Base function: add entries.

---
+++ TimeJournal/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using ReactiveUI;
+
+namespace TimeJournal.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private string _currentText = string.Empty;
+    private ObservableCollection<JournalEntry> _entries;
+
+    public MainWindowViewModel()
+    {
+        Entries = [];
+        AddEntryCommand = ReactiveCommand.Create(AddEntry);
+        AddNewLineCommand = ReactiveCommand.Create(AddNewLine);
+    }
+
+    public string CurrentText
+    {
+        get => _currentText;
+        set => this.RaiseAndSetIfChanged(ref _currentText, value);
+    }
+
+    public ObservableCollection<JournalEntry> Entries
+    {
+        get => _entries;
+        set => this.RaiseAndSetIfChanged(ref _entries, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> AddEntryCommand { get; }
+    public ReactiveCommand<Unit, Unit> AddNewLineCommand { get; }
+
+    private void AddEntry()
+    {
+        if (!string.IsNullOrWhiteSpace(CurrentText))
+        {
+            Entries.Add(new JournalEntry
+            {
+                Text = CurrentText,
+                Timestamp = DateTime.Now
+            });
+            CurrentText = string.Empty;
+        }
+    }
+
+    private void AddNewLine()
+    {
+        CurrentText += Environment.NewLine;
+    }
+}
+
+public class JournalEntry
+{
+    public string Text { get; set; }
+    public DateTime Timestamp { get; set; }
+}
---
+++ TimeJournal/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace TimeJournal.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
