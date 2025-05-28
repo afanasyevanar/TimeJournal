48c44b3c1cddab6960a52f22e4bbd31832cf58c9
afanasyevanar <afanasyevanar@gmail.com>
2025-05-28 12:27:52 +0700

    Add window restore by timer

--- TimeJournal/Views/MainWindow.axaml.cs
+++ TimeJournal/Views/MainWindow.axaml.cs
@@ -1,11 +1,23 @@
 using Avalonia.Controls;
+using System;
+using System.Timers;
+using Avalonia.Threading;
 
 namespace TimeJournal.Views;
 
 public partial class MainWindow : Window
 {
-  public MainWindow()
-  {
-    InitializeComponent();
-  }
+    private readonly DispatcherTimer _windowTimer;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        _windowTimer = new DispatcherTimer(TimeSpan.FromMinutes(20), DispatcherPriority.Background, OnTimerElapsed);
+    }
+
+    private void OnTimerElapsed(object? sender, EventArgs e)
+    { 
+        WindowState = WindowState.Normal; 
+    }
 }
